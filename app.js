b99750c1a91d22da59746a36aea5181dd64811f6
Kyanij Maharjan <mkyanij1@gmail.com>
2019-06-08 23:05:46 +0545

    create procfile and changed port

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cors());
 
 app.use('/poll', poll)
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 // Start server
 app.listen(port, () => console.log(`Server running at ${port}`));
 
